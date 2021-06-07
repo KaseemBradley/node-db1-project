5469b4dbd1759efd46344533d16028fd88328fa8
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-07 19:20:51 -0400

    created models for getting all accounts and for getting a single account by id / utilized models in endpoints for get requests

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,46 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const db = require("../../data/db-config");
+const middleware = require("./accounts-middleware");
+const Account = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const accounts = await Account.getAll();
+    res.json(accounts);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:id', (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const accountWithId = await Account.getById(req.params.id);
+    res.json(accountWithId);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.delete("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
+
+// eslint-disable-next-line
+router.use((err, req, res, next) => {
+  // DO YOUR MAGIC
+  res.status(err.status || 404).json({
+    message: err.message,
+  });
+});
 
 module.exports = router;
